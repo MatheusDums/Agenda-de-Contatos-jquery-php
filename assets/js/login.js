2432ac23901c73c8fa238a0cbd0d528390096195
MatheusDums <dumss@icloud.com>
2025-07-11 08:16:02 -0300

    atualizando arquivos

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,9 +3,12 @@ const areaResp = document.getElementById("col_resp");
  
 
 loginForm.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     if(document.getElementById("email_input").value === "") {
-        areaResp.innerHTML = "<div class='alert alert-danger text-center alert-dismissible fade show' role='alert'>Email ou Senha Invalido.<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
+        areaResp.innerHTML = "<div class='alert alert-danger text-center alert-dismissible fade show' role='alert'>Preencha o campo Email.<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
+    } else if(document.getElementById("senha_input").value === "") {
+        areaResp.innerHTML = "<div class='alert alert-danger text-center alert-dismissible fade show' role='alert'>Preencha o campo Senha.<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
     }
 })
